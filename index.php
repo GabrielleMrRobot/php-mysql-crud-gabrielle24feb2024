3dacdd19cb5df5ceb1e24154e874630ba94c2639
francofrijolish <francomendoza260@gmail.com>
2024-03-09 17:46:26 -0400

    feat: finish index.php

--- index.php
+++ index.php
@@ -19,53 +19,49 @@
 
     <!-- ADD TASK FORM-->
     <div class="card card-body">
-      <form action="save_task.php" method="POST">
-        <div class="form-group">
-        <input type="test" name="title" class="form-control" placeholder="Task Title"  autofocus>
-        </div>
-        <div class="form-group">
-          <textarea name="description" id="" cols="30" rows="2" class="form-control" placeholder="Task-Description">
-          </textarea>
-        </div>
-        <input type="submit" name="save_task" class="btn btn-success btnblock" value="Save Task">
-        <div class="col-md-8">
-          <table class="table-bordered">
-            <thead>
-              <tr>
-              <th>title<th>
-              <th>description<th>
-              <th>Created At<th>
-              <th>Action<th>
-                <tr>
-      </thead>
-      <tbody>
-        <?php
-        $query= "SELECT * FROM task";
-        $result_tasks = mysqli_query($conn, $query);
-
-        while($row = mysqli_fetch_assoc($result_tasks)) { ?>
-       <tr>
-        <td><?php echo$row['title']; ?></td>
-        <td><?php echo$row['description']; ?></td>  
-        <td><?php echo$row['created_at']; ?></td>    
-        <td>
-          <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary"></a>
-          <i class="fas fa-marker"></i>
-        </a>
-        <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-danger"></a>
-          <i class="fas fa-trash-alt"></i>
-        </a>
-        </td>
-        </tr>
-
-        <?php } ?>
-        </tbody>
-        </table>
-
-    
-
-      </form>
+        <form action="save_task.php" method="POST">
+          <div class="form-group">
+            <input type="test" name="title" class="form-control" placeholder="Task Title" autofocus>
+          </div>
+          <div class="form-group">
+            <textarea name="description" cols="30" rows="2" class="form-control" placeholder="Task Description"></textarea>
+          </div>
+          <input type="submit" name="save_task" class="btn btn-success btn-block" value="Save Task">
+        </form>
+      </div>
     </div>
+    <div class="col-md-8">
+      <table class="table-bordered">
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Description</th></th>
+            <th>Created At</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+        <tbody>
+          <?php
+          $query = "SELECT * FROM task";
+          $result_tasks = mysqli_query($conn, $query);
+
+          while($row = mysqli_fetch_assoc($result_tasks)) { ?>
+          <tr>
+            <td><?php echo$row['title']; ?></td>
+            <td><?php echo$row['description']; ?></td>
+            <td><?php echo$row['created_at']; ?></td>
+            <td>
+              <a href="edit.php?id=<?php echo $row['id']?>" class="btn btn-secondary">
+                <i class="fas fa-marker"></i>
+              </a>
+              <a href="delete_task.php?id=<?php echo $row['id']?>" class="btn btn-danger">
+                <i class="far fa-trash-alt"></i>
+              </a>
+            </td>
+          </tr>
+          <?php } ?>
+        </tbody>
+      </table>
     </div>
   </div>
 </main>
